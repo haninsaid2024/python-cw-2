987859c6134cd1c7a42deedc02e3892d30458725
haninsaid2024 <haninfouda077@gmail.com>
2023-09-03 13:47:13 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+my_name = input (" enter your name")
+my_age = int(input(("enter your age"))
+print(f"my name is {my_name} and I am {my_age} years old")
+
+first_number = int(input("Enter first number :"))
+second_numbrr = int(input("Enter second number:"))
+operation = input("Enter operation type:")
+If operation =="+":
+    print(first_number + second_number)
+elif operation =="-":
+     print(first_number - second_number)
+elif operation == "*"
+     print(first_number * second_number)
+elif operation == "/":
+     pirnt(first_number / second_number)
+else:
+     print("The operation is  not valid")
